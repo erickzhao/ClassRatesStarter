9b11963926a8b621324c4f5247b98cb4e926db01
erick <erick@hotmail.ca>
2018-03-28 19:32:13 -0400

    Add code

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -3,6 +3,37 @@
 // API Documentation - https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics.V2.0/operations/56f30ceeeda5650db055a3c9
 function submitComment(commentControl) {
     // TODO - Call API
+    const comments = document.getElementsByName(commentControl)[0].value;
+    const base = 'https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
+    const apiKey = 'a067b8a78745477782fbd19254624913';
+
+    const body = {
+        'documents': [
+            {
+                language: 'en-US',
+                id: 1,
+                text: comments
+            }
+        ]
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: base,
+        data: JSON.stringify(body),
+        processData: false,
+        headers: {
+            'Ocp-Apim-Subscription-Key': apiKey,
+            'Content-Type': 'application/json'
+        }
+    }).done((result) => {
+        const score = result.documents[0].score;
+        const rating = Math.round(score*4)+1;
+        const currentItemId = JSON.parse(localStorage.getItem('currentItemId'));
+        window.location.href = `/comments?classId=${currentItemId}&rating=${rating}&comments=${comments}`;
+    }).fail((xhr, status, err) => {
+        alert(err);
+    });
 }
 
 function navigateToComments(classId) {
